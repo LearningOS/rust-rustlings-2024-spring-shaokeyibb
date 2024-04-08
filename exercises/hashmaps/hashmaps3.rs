fa1ef15ed4073cc31e8ffa31ca2a71522c5e0729
HikariLan <shaokeyibb@126.com>
2024-04-08 10:12:15 +0000

    complete to quiz2 in 47/110

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,6 +37,16 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if let Option::Some(team) = scores.get(&team_1_name){
+            scores.insert(team_1_name, Team{goals_scored: team_1_score+team.goals_scored, goals_conceded: team_2_score+team.goals_conceded});
+        } else {
+            scores.insert(team_1_name, Team{goals_scored: team_1_score, goals_conceded: team_2_score});
+        }
+        if let Option::Some(team) = scores.get(&team_2_name){
+            scores.insert(team_2_name, Team{goals_scored: team_2_score+team.goals_scored, goals_conceded: team_1_score+team.goals_conceded});
+        } else {
+            scores.insert(team_2_name, Team{goals_scored: team_2_score, goals_conceded: team_1_score});
+        }
     }
     scores
 }
--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -7,12 +7,10 @@
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&mut vec0);
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
@@ -21,12 +19,10 @@ fn main() {
     println!("{} has length {}, with contents `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+    (*vec).push(22);
+    (*vec).push(44);
+    (*vec).push(66);
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
-
-    vec
+    (*vec.clone()).to_vec()
 }
