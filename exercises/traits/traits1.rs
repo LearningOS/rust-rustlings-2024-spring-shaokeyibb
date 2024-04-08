6c466cf121b3b1df3ed7fb7de411e7f12356f5f4
HikariLan <shaokeyibb@126.com>
2024-04-08 13:05:29 +0000

    complete to quiz3 in 63/110

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,13 +7,14 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
+    fn append_bar(self) -> Self {
+        self + "Bar"
+    }
     // TODO: Implement `AppendBar` for type `String`.
 }
 
